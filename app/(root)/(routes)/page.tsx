1b873431d3ede17da7daef3e95ee5e68495533d4
Mohammed Harris <md.harry.98@gmail.com>
2023-08-24 13:11:58 -0400

    category filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,16 @@
+import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
-import { UserButton } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
 
-const RootPage = () => {
+const RootPage = async() => {
+
+    const categories = await prismadb.category.findMany();
+    
     return ( 
         <div className="h-full p-4 space-y-2">
 
             <SearchInput />
+            <Categories data={categories} />
         </div>
      );
 }
