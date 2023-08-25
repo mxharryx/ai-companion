07889ca3971c7bb117cd90133f7da8050d16e64c
Mohammed Harris <md.harry.98@gmail.com>
2023-08-25 18:34:18 -0400

    created companions list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,36 @@
 import { Categories } from "@/components/categories";
+import { Companions } from "@/components/companions";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async() => {
+interface RootPageProps {
+    searchParams:{
+        categoryId: string;
+        name: string;
+    }
+}
+
+const RootPage = async({
+    searchParams
+}: RootPageProps) => {
+    const data = await prismadb.companion.findMany({
+        where:{
+            categoryId: searchParams.categoryId,
+            name:{
+                search: searchParams.name
+            }
+        },
+        orderBy:{
+            createdAt: "desc",
+        },
+        include:{
+            _count:{
+                select:{
+                    messages: true
+                }
+            }
+        }
+    })
 
     const categories = await prismadb.category.findMany();
     
@@ -11,6 +39,7 @@ const RootPage = async() => {
 
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
      );
 }
