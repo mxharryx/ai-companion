758d6202bd746d1098d3278dc88fca5d6145c8ff
Mohammed Harris <md.harry.98@gmail.com>
2023-08-25 18:00:23 -0400

    companion api fix

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -13,6 +13,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -61,6 +63,9 @@ export const CompanionForm = ({
     initialData
 }: CompanionFormProps) => {
 
+    const router = useRouter();
+    const{toast} = useToast();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
@@ -83,8 +88,18 @@ export const CompanionForm = ({
                 //Create companion functionality
                 await axios.post("/api/companion", values);
             }
+
+            toast({
+                description: "Success."
+            });
+
+            router.refresh();
+            router.push("/");
         }catch(error){
-            console.log(error, 'something went wrong');
+            toast({
+                variant:"destructive",
+                description:"Something went wrong",
+            });
         }
     }
   return (
