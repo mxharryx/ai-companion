3af952eda100ff67a5ac6211a8e22c3df17f5601
Mohammed Harris <md.harry.98@gmail.com>
2023-08-25 03:40:43 -0400

    installed cloudinary and loaded env file

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
 
 interface CompanionFormProps{
@@ -59,7 +59,7 @@ export const CompanionForm = ({
     <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
      <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 pb-10">
-            <div className="space-y-2 w-full col-span-2">
+            <div className="space-y-2 w-full">
                 <div>
                     <h3 className="text-lg font-medium">
                         General Information
@@ -77,6 +77,7 @@ export const CompanionForm = ({
                         <FormControl>
                             Image Upload Component
                         </FormControl>
+                        <FormMessage />
                     </FormItem>
                 )}
             />
