37ebc44a640e9983c068c7041743268bcd436a07
Mohammed Harris <md.harry.98@gmail.com>
2023-08-25 19:52:56 -0400

    chat header comp

---
+++ components/chat-header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Companion, Message } from "@prisma/client";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface ChatHeaderProps{
+    companion: Companion &{
+        messages: Message[];
+        _count:{
+            messages: number;
+        };
+    };
+};
+
+export const ChatHeader = ({
+    companion
+}: ChatHeaderProps) => {
+
+    const router = useRouter();
+  return (
+    <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+        <div className="flex gap-x-2 items-center">
+            <Button onClick={()=>router.back()} size="icon" variant="ghost">
+                <ChevronLeft className="h-8 w-8"/>
+            </Button>
+        </div>
+    </div>
+  )
+}
