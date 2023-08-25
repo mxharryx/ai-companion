0d221b7cbeaad17c8f444e6dd4c74ad0418ff03f
Mohammed Harris <md.harry.98@gmail.com>
2023-08-25 03:43:30 -0400

    image upload component

---
+++ components/image-upload.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+interface ImageUploadProps{
+    value: string;
+    onChange: (src: string) => void;
+    disabled?: boolean;
+};
+
+
+export const ImageUpload = () => {
+  return (
+    <div>image-upload</div>
+  )
+}
+
+
