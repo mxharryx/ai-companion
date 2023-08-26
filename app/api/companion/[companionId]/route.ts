fe91cc624e9ebdefd1213b0a3d77c89e06f8da48
Mohammed Harris <md.harry.98@gmail.com>
2023-08-25 20:34:33 -0400

    chat header, edit and delete feat

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,5 +1,5 @@
 import prismadb from "@/lib/prismadb";
-import { currentUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function PATCH(req: Request, {params}:{params: {companionId:string}}){
@@ -44,4 +44,30 @@ export async function PATCH(req: Request, {params}:{params: {companionId:string}
         console.log("[COMPANION_PATCH]", error);
         return new NextResponse("Internal Error", {status:500});
     }
+}
+
+
+export async function DELETE(request: Request, {params}: {params: {companionId:string}}) {
+    try{
+        const {userId} = auth();
+
+        if(!userId){
+            return new NextResponse("Unauthorized", {status: 401});
+        }
+
+        const companion = await prismadb.companion.delete({
+            where:{
+                userId,
+                id: params.companionId,
+            }
+        });
+        
+        return NextResponse.json(companion);
+
+    } catch(error){
+        console.log("[COMPANION_DELETE]", error);
+        return new NextResponse("Internal Error", {status:500});
+
+    }
+
 }
