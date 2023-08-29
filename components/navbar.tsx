39ad0e4afabeb499c95db8d3719389d395f00ed1
Mohammed Harris <md.harry.98@gmail.com>
2023-08-29 10:45:50 -0400

    Stripe checkout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {ModeToggle} from "@/components/mode-toggle";
 import { MobileSideBar } from "@/components/mobile-sidebar";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 const font = Poppins({
     weight:"600",
@@ -15,6 +16,8 @@ const font = Poppins({
 });
 
 export const Navbar = () => {
+
+    const proModal = useProModal();
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
         <div className="flex items-center">
@@ -26,7 +29,7 @@ export const Navbar = () => {
             </Link> 
         </div>
         <div className="flex items-center gap-x-3">
-            <Button variant="premium" size="sm">
+            <Button onClick={proModal.onOpen} variant="premium" size="sm">
                 Upgrade
                 <Sparkles className="h-4 w-4 fill-white text-white ml-2" /> 
             </Button>
