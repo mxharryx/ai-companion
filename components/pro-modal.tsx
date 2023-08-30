ac4722955ef49c0bd0539b2f1f866f94f0421128
Mohammed Harris <md.harry.98@gmail.com>
2023-08-30 13:21:05 -0400

    Dialog close fix

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,46 +1,51 @@
 "use client";
 
-import axios from "axios";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { useProModal } from "@/hooks/use-pro-modal";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
-import { useToast } from "@/components/ui/use-toast";
 import { useEffect, useState } from "react";
+import axios from "axios";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useToast } from "@/components/ui/use-toast";
 
 export const ProModal = () => {
-
     const proModal = useProModal();
-    const {toast} = useToast();
-
-    const [loading, setLoading] = useState(false);
-
     const [isMounted, setIsMounted] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const { toast } = useToast();
 
-    useEffect(() =>{
+    useEffect(() => {
         setIsMounted(true);
-    }, [])
+    }, []);
 
-    const onSubscribe = async()=>{
-        try{
+
+    const onSubscribe = async () => {
+        try {
             setLoading(true);
             const response = await axios.get("/api/stripe");
 
             window.location.href = response.data.url;
-        }catch(error){
+        } catch (error) {
             toast({
+                description: "Something went wrong",
                 variant: "destructive",
-                description: "Something went wrong"
-            }) 
-        }finally{
+            });
+        } finally {
             setLoading(false);
         }
     }
 
-    if(!isMounted){
+    if (!isMounted) {
         return null;
     }
-
 
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -50,22 +55,21 @@ export const ProModal = () => {
                         Upgrade to Pro
                     </DialogTitle>
                     <DialogDescription className="text-center space-y-2">
-                        Create <span className="text-sky-500 mx-1 font-medium">Custom AI</span> Companions
+                        Create
+                        <span className="text-sky-500 mx-1 font-medium">Custom AI</span>
+                        Companions!
                     </DialogDescription>
                 </DialogHeader>
                 <Separator />
                 <div className="flex justify-between">
                     <p className="text-2xl font-medium">
-                        $9
-                        <span className="text-sm font-normal">
-                            .99 / mo
-                        </span>
+                        $9<span className="text-sm font-normal">.99 / mo</span>
                     </p>
-                    <Button disabled={loading} onClick={onSubscribe} variant="premium">
+                    <Button onClick={onSubscribe} disabled={loading} variant="premium">
                         Subscribe
                     </Button>
                 </div>
             </DialogContent>
         </Dialog>
-    )
-}
+    );
+};
--- hooks/use-pro-modal.tsx
+++ hooks/use-pro-modal.tsx
@@ -9,5 +9,5 @@ interface useProModalStore {
 export const useProModal = create<useProModalStore>((set) => ({
     isOpen: false,
     onOpen: () => set({isOpen: true}),
-    onClose:()=>set({isOpen: true}),
+    onClose:()=>set({isOpen: false}),
 }));
