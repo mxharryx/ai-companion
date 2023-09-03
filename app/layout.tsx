c44cf6b3f2627d27ceac209fd3e7361499944138
Mohammed Harris <md.harry.98@gmail.com>
2023-09-03 11:21:53 -0400

    metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,15 @@ export default function RootLayout({
         <meta name="author" content="AI, Chatbot, Mohammed Harris, Software Development, Cloud Development, Game Development" />
         <meta property="og:title" content="AI Companion | Chat with anyone!" />
         <meta property="og:description" content="Create Custom AI | A project by Mohammed Harris" />
-        <meta property="og:image" content="https://ai-companion-black.vercel.app/public/home_page.png" />
+        <meta property="og:image" content="https://ai-companion-black.vercel.app/home_page.png" />
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://ai-companion-black.vercel.app" />
+        <meta property="twitter:image" content="https://ai-companion-black.vercel.app/home_page.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="AI Companion | Chat with anyone!" />
+        <meta property="twitter:description" content="Create Custom AI | A project by Mohammed Harris" />
+        <meta name="twitter:image:alt" content="AI Companion's Landing Page"/>
+        <meta name="twitter:creator" content="@mohammedharris_"/>
         </Head>
         <body className={cn("bg-secondary", inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
