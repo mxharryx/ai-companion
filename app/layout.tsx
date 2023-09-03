b864aac3146923f28b836f1b3ff81931ec952e97
Mohammed Harris <md.harry.98@gmail.com>
2023-09-03 11:14:15 -0400

    homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
 import { Toaster } from '@/components/ui/toaster'
 import { ProModal } from '@/components/pro-modal'
+import Head from 'next/head'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -24,6 +25,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
+        <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="AI, Chatbot, Mohammed Harris, Software Development, Cloud Development, Game Development" />
+        <meta property="og:title" content="AI Companion | Chat with anyone!" />
+        <meta property="og:description" content="Create Custom AI | A project by Mohammed Harris" />
+        <meta property="og:image" content="https://ai-companion-black.vercel.app/public/home_page.png" />
+        </Head>
         <body className={cn("bg-secondary", inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ProModal />
